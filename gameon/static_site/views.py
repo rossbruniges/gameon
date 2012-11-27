5ea80813e55f28c61adcca39bcc905d5be218e5c
Ross Bruniges <rossbruniges@gmail.com>
2012-11-27 12:59:44 +0000

    Lots and lots of static content - unless Matt says different all we have left is the FAQ page to go

--- gameon/static_site/views.py
+++ gameon/static_site/views.py
@@ -2,50 +2,49 @@ from django.shortcuts import render
 
 import commonware
 
+from gameon.submissions.models import Category
+
 log = commonware.log.getLogger('playdoh')
 
 
 def home(request, template='static_site/landing.html'):
     """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
-    log.debug("I'm home!")
+    data = {}
     return render(request, template, data)
 
 
 def rules(request, template='static_site/rules.html'):
     """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
-    log.debug("I'm reading the rules!")
+    categories = Category.objects.all()
+    data = {
+        'hackable': categories.get(slug='hackable-games'),
+        'learning': categories.get(slug='learning-games'),
+        'mobile': categories.get(slug='mobile-games'),
+    }
     return render(request, template, data)
 
 
 def judges(request, template='static_site/judges.html'):
     """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
+    data = {}
+    return render(request, template, data)
+
+
+def judging(request, template='static_site/judging.html'):
+    """Main example view."""
+    data = {}
     return render(request, template, data)
 
 
 def prizes(request, template='static_site/prizes.html'):
     """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
+    data = {}
     return render(request, template, data)
 
 
 def resources(request, template='static_site/resources.html'):
     """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
-    return render(request, template, data)
-
-
-def previous(request, template='static_site/previous.html'):
-    """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
-    return render(request, template, data)
-
-
-def categories(request, template='static_site/categories.html'):
-    """Main example view."""
-    data = {}  # You'd add data here that you're sending to the template.
+    data = {}
     return render(request, template, data)
 
 
