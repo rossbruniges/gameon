3a2420a3b9e94e8bdcdf157951b8a29fab390a0d
Atul Varma <varmaa@gmail.com>
2012-11-30 11:28:52 -0500

    added submissions.models.url2embed(), which (hopefully) works securely.

--- gameon/submissions/test_models.py
+++ gameon/submissions/test_models.py
@@ -2,6 +2,25 @@ import test_utils
 
 from . import models
 
+class Url2EmbedTests(test_utils.TestCase):
+    def test_falsy_value_returns_none(self):
+        self.assertEqual(models.url2embed(None), None)
+        
+    def test_arbitrary_url_returns_none(self):
+        self.assertEqual(models.url2embed("http://foo.org/blah"), None)
+
+    def test_vimeo_url_works(self):
+        self.assertEqual(models.url2embed("http://vimeo.com/channels/staffpicks/34062273"),
+                         '<iframe src="https://player.vimeo.com/video/34062273?title=0&amp;byline=0&amp;portrait=0&amp;badge=0" width="640" height="385" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>')
+
+    def test_short_youtube_url_works(self):
+        self.assertEqual(models.url2embed("http://youtu.be/ITTxTCz4Ums"),
+                         '<iframe class="youtube-player" type="text/html" width="640" height="385" src="https://www.youtube.com/embed/ITTxTCz4Ums" frameborder="0"></iframe>')
+
+    def test_long_youtube_url_works(self):
+        self.assertEqual(models.url2embed("http://www.youtube.com/watch?v=ITTxTCz4Ums"),
+                         '<iframe class="youtube-player" type="text/html" width="640" height="385" src="https://www.youtube.com/embed/ITTxTCz4Ums" frameborder="0"></iframe>')
+    
 class EntryFeatureVideoUrlTests(test_utils.TestCase):
     def _repr(self, model):
         return ("Entry(video_url=%s)"
