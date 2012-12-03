5d0a6ad5ef533fa36e0aca5dd023e265a2562ca2
Ross Bruniges <rossbruniges@gmail.com>
2012-12-03 15:50:23 +0000

    A lot of work required to change a category title

--- gameon/static_site/views.py
+++ gameon/static_site/views.py
@@ -15,11 +15,8 @@ def home(request, template='static_site/landing.html'):
 
 def rules(request, template='static_site/rules.html'):
     """Main example view."""
-    categories = Category.objects.all()
     data = {
-        'hackable': categories.get(slug='hackable-games'),
-        'learning': categories.get(slug='learning-games'),
-        'mobile': categories.get(slug='cross-platform-games'),
+        'categories': Category.objects.all().order_by('name'),
     }
     return render(request, template, data)
 
